824a5bdf652034e966b7528e1445ec8671929f33
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-27 07:27:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  # triangle code
+  def initialize(a, b, c)
+    if(a <= 0 || b <= 0 || c <= 0)
+      
+    end
+    
+    if((a + b > c && a + c > b && b + c > a ) == false) 
+      
+    end
+    
+    
+  end
+  
+  def kind
+    if(a == b == c)
+      :
+    end
+  end
+ 
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
